7dd61a2a3ef2b39cb6b2963788f86bd5344251aa
william1nguyen <vietanhhd03@gmail.com>
2025-02-21 00:41:45 +0700

    feat(notification): setup react-toastify

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,11 +1,19 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { MainPage } from "./pages/MainPage";
+import { UploadPage } from "./pages/UploadPage";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <>
-      
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="" element={<MainPage />} />
+        <Route path="/upload" element={<UploadPage />} />
+      </Routes>
+      <ToastContainer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
