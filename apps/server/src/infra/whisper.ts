8ffd3f249e14bc3bdf77b903966d6ff8a0bd552e
william1nguyen <vietanhhd03@gmail.com>
2025-03-06 17:19:45 +0700

    fix(server): update whisper service

---
+++ apps/server/src/infra/whisper.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { env } from "~/env";
+import FormData from "form-data";
+import logger from "./logger";
+
+export const whisperHttpClient = axios.create({
+  baseURL: env.WHISPERAI_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const transcribe = async (buffer: Buffer, filename = "filename.mp4") => {
+  try {
+    const form = new FormData();
+
+    form.append("files", buffer, {
+      filename,
+      contentType: "video/mp4",
+      knownLength: buffer.length,
+    });
+
+    const res = await whisperHttpClient.post("/transcribe", form, {
+      headers: {
+        ...form.getHeaders(),
+        Accept: "application/json",
+      },
+    });
+    const data = res.data;
+
+    return data.results;
+  } catch (err) {
+    logger.error(`Failed to get transcript ${err}`);
+    return null;
+  }
+};
