6248494144f7aae85fe96b5d00edfde3a5a7a7cf
william1nguyen <vietanhhd03@gmail.com>
2025-07-23 01:33:16 +0700

    fix(server): remove ping service

--- apps/server/src/infra/jobs/index.ts
+++ apps/server/src/infra/jobs/index.ts
@@ -1,21 +1,14 @@
 import logger from "../logger";
-import {
-  createPingWorker,
-  pingQueue,
-  startCronPingJob,
-} from "./workers/pingGemini";
 import { createSummaryWorker, summaryQueue } from "./workers/summarize";
 
 const initWorkers = () => {
   logger.info("Create workers");
-  createPingWorker();
   createSummaryWorker();
   logger.info("Workers created");
 };
 
 export const setupBackgroundJobs = () => {
   initWorkers();
-  startCronPingJob();
 };
 
-export const queues = [pingQueue, summaryQueue];
+export const queues = [summaryQueue];
