0c748cd358abca9668a3073f67904a6d519ea209
william1nguyen <vietanhhd03@gmail.com>
2026-03-03 02:52:10 +0700

    feat(web): configure React Router v7, Vite, Tailwind, shadcn/ui

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,11 +1,12 @@
 import "./index.css";
-import App from "./App.tsx";
 import { createRoot } from "react-dom/client";
+
+import App from "./App.tsx";
 import { KeycloakAuthProvider } from "./providers/KeycloakAuthProvider.tsx";
 import "./i18n";
 
 createRoot(document.getElementById("root")!).render(
   <KeycloakAuthProvider>
     <App />
-  </KeycloakAuthProvider>
+  </KeycloakAuthProvider>,
 );
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,12 +1,8 @@
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "tailwindcss";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss()],
-    },
-  },
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
